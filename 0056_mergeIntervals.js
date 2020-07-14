9dfa2cd832c80bb1bfc4dc24c6c257b9ce9a8dd1
Charles Buczel <charles.buczel@gmail.com>
2020-07-14 01:50:45 +0100

    Add jsdocs

--- 0056_mergeIntervals.js
+++ 0056_mergeIntervals.js
@@ -1,6 +1,10 @@
 /**
- * @param {number[][]} intervals
- * @return {number[][]}
+ * @param {number[][]} intervals Array of intervals.
+ * @return {number[][]} Array of intervals after merging all that were overlapping.
+ * @summary Merge Intervals {@link https://leetcode.com/problems/merge-intervals/}
+ * @description Given an array of intervals (start, finish), return array with overlapping intervals merged.
+ * Space O(1) - depending on js engine sort implementation (can it be done in place?).
+ * Time O(nlogn) - sort (logn) + one iteration of n elements. Depends on js engine sort implementation though.
  */
 const merge = intervals => {
     intervals.sort((a, b) => a[0] > b[0] ? 1 : -1);
--- 0680_validPalindromeII.js
+++ 0680_validPalindromeII.js
@@ -1,8 +1,23 @@
 /**
- * @param {string} word
- * @return {boolean}
+ * @param {string} word Input string to check.
+ * @return {boolean} Can string be a palindrome with at least one character deleted.
+ * @summary Valid Palindrome II {@link https://leetcode.com/problems/valid-palindrome-ii/}
+ * @description Given a string, can it be a palindrome with at most one character removed.
+ * Space O(1) - no extra memory used.
+ * Time O(n) - iterate once over a list, perform up to two check (each is n).
  */
-const validPalindrome = (word) => { 
+const validPalindrome = word => { 
+    const isPalindrome = (word, left, right) => {
+        while (left < right) {
+            if (word[left] !== word[right]) return false;
+    
+            left++;
+            right--;
+        }
+    
+        return true;
+    }
+
     let left = 0;
     let right = word.length - 1;
 
@@ -17,14 +32,3 @@ const validPalindrome = (word) => {
 
     return true;
 };
-
-const isPalindrome = (word, left, right) => {
-    while (left < right) {
-        if (word[left] !== word[right]) return false;
-
-        left++;
-        right--;
-    }
-
-    return true;
-}
