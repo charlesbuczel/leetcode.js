81ba9a563bc7b8d3cf9b6d5e57024063c371c8be
Charles Buczel <charles.buczel@gmail.com>
2020-08-01 17:38:08 +0100

    Valid Palindrome

---
+++ 0125_validPalindrome.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+/**
+ * @param {strng} s string to check if palindrome
+ * @return {boolean} True if string is a palindrome (only counting alphanumeric chars).
+ * @summary Valid palindrome {@link https://leetcode.com/problems/valid-palindrome/}
+ * @description Given an input string, return true if its a palindrome (non-alphanumeric chars excluded);
+ * Space O(1) - No extra space (few variables).
+ * Time O(n) - Traverse over the input string.
+ */
+const isPalindrome = s => {
+  if (!s) return true;
+
+  const isAlphanumeric = c =>
+    (code >= 'a'.charCodeAt() && code <= 'z'.charCodeAt()) || (code >= '0'.charCodeAt() && code <= '9'.charCodeAt());
+
+  let left = 0;
+  let right = s.length - 1;
+  const mid = Math.ceil(s.length / 2);
+
+  while (left <= right) {
+    const leftChar = s[left].toLowerCase();
+    const rightChar = s[right].toLowerCase();
+
+    const isLeftValid = isAlphanumeric(leftChar.charCodeAt());
+    const isRightValid = isAlphanumeric(rightChar.charCodeAt());
+
+    if (isLeftValid && isRightValid && leftChar !== rightChar) return false;
+
+    if (!isLeftValid && isRightValid) left++;
+    else if (isLeftValid && !isRightValid) right--;
+    else {
+      left++;
+      right--;
+    }
+  }
+
+  return true;
+};
